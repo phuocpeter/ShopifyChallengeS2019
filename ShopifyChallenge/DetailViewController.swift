692fd6ea48f8a2a2aa4d92453a43a80dc87043da
Phuoc Tran <phuocpeter19@gmail.com>
2019-01-14 23:27:12 -0500

    Initial Commit

---
+++ ShopifyChallenge/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ShopifyChallenge
+//
+//  Created by Phuoc Tran on 2019-01-14.
+//  Copyright © 2019 Phuoc Tran. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
